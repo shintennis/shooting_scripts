66e8e06e4a1e084c0dc2c5ba7659af490041311a
shintennis <shin.tennis.1126@gmail.com>
2022-04-11 18:45:45 +0900

    score db connect

--- DB_connect/DB_Score.cs
+++ DB_connect/DB_Score.cs
@@ -6,10 +6,11 @@ using UnityEngine.Networking;
 
 public class DB_Score : MonoBehaviour
 {
-    WWWForm wwwform;
     
     [SerializeField] Button scoreButton;
     
+    [SerializeField] Text scoreText;
+
     public string url = "http://localhost:8080/Unity/userScores.php";
     
     
@@ -20,7 +21,7 @@ public class DB_Score : MonoBehaviour
 
     IEnumerator Scores()
     {
-        wwwform = new WWWForm();
+        WWWForm form = new WWWForm();
         Score score = new Score();
         DB_user sendData = new DB_user(); 
         
@@ -28,20 +29,20 @@ public class DB_Score : MonoBehaviour
         sendData.user_id = 11;
         sendData.score = 3000;
 
-        wwwform.AddField("user", JsonUtility.ToJson(sendData));
-        using (UnityWebRequest www = UnityWebRequest.Post(url,  wwwform))
+        form.AddField("user_id", sendData.user_id);
+        form.AddField("score", sendData.score);
+        using (UnityWebRequest request = UnityWebRequest.Post(url,  form))
         {
-            yield return www.SendWebRequest();
+            yield return request.SendWebRequest();
 
-            if (!string.IsNullOrEmpty(www.error))
+            if (request.result == UnityWebRequest.Result.ProtocolError || request.result == UnityWebRequest.Result.ConnectionError)
             {
-                Debug.Log("error:" + www.error);
+                Debug.Log("error:" + request.result);
                 yield break;
             }
-            Debug.Log("text:" + www.downloadHandler.text);
+            // Debug.Log(request.downloadHandler.text);
+            scoreText.text = request.downloadHandler.text;
             Debug.Log("<color=yellow>" + "connect" + "</color>");
-            DB_user user = JsonUtility.FromJson<DB_user>(www.downloadHandler.text);
-            Debug.Log("id:"+user.id + ", user_id:"+user.user_id + ", score:"+user.score);
         }
     }
 }
